518a5405b263ee7ff0b0b4cd0d1e242d74904aae
dens0n <156082076+dens0n@users.noreply.github.com>
2024-10-31 11:37:04 +0100

    ISSUE-123 #time 2h #comment Fixade buggen

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,19 +1,20 @@
-import AcmeLogo from "../ui/acme-logo";
-import LoginForm from "../ui/login-form";
-import { Metadata} from "next";
+import AcmeLogo from '../ui/acme-logo';
+import LoginForm from '../ui/login-form';
+import { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: 'Login',
 };
 
-
+//test
+//test
 //test
 
 export default function LoginPage() {
   return (
     <main className="flex items-center justify-center md:h-screen">
       <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
-        <div className="flex h20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
+        <div className="h20 flex w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
           <div className="w-32 text-white md:w-36">
             <AcmeLogo />
           </div>
@@ -21,5 +22,5 @@ export default function LoginPage() {
         <LoginForm />
       </div>
     </main>
-  )
+  );
 }
