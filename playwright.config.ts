eba2aea3e5658d08c6bbcd1a71a07342da931574
JohanTran <johan.tran0210@gmail.com>
2024-11-07 14:29:05 +0100

    changed url to localhost

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
 
     use: {
         // Base URL to use in actions like `await page.goto('/')`.
-        baseURL: 'http://127.0.0.1:3000',
+        baseURL: 'localhost:3000',
 
         // Collect trace when retrying the failed test.
         trace: 'on-first-retry',
@@ -35,8 +35,8 @@ export default defineConfig({
     ],
     // Run your local dev server before starting the tests.
     webServer: {
-        command: 'npm run start',
-        url: 'http://127.0.0.1:3000',
+        command: 'npm run dev',
+        url: 'localhost:3000',
         reuseExistingServer: !process.env.CI,
     },
 });
